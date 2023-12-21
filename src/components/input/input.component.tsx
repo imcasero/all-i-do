b2519afa68b6eebd675caab6601c64ea5a320f41
Imcasero <diegocaserosmr@gmail.com>
2023-12-21 20:44:13 +0100

    feat: responsive design and new icon whit new header

--- src/components/input/input.component.tsx
+++ src/components/input/input.component.tsx
@@ -21,8 +21,8 @@ export const InputComponent: React.FC<InputComponentProps> = ({ onAddTask }) => 
 
 
     return (
-        <section className=" min-w-full flex justify-center mt-4">
-            <form className="min-w-full flex justify-center gap-6" onSubmit={handleSubmit}>
+        <section className=" min-w-full flex justify-center mt-4 lg:px-0 px-5">
+            <form className="min-w-full flex justify-center lg:gap-6 gap-2" onSubmit={handleSubmit}>
                 <input className="w-3/4 rounded-lg border-none py-2 px-4 text-textColorDark" type="text" placeholder="add a new task" value={title} onChange={onChangeTitle} />
                 <button className="w-auto flex items-center bg-primaryColor text-textColorDark py-2 px-4 rounded-lg border-none gap-2" type="submit" >
                     Add
--- src/components/tasks/tasks.component.tsx
+++ src/components/tasks/tasks.component.tsx
@@ -10,7 +10,7 @@ export const TasksComponent: React.FC<TaskComponentProps> = ({ children, data })
     const completedTaskLength = data.filter((task) => task.isCompleted).length;
 
     return (
-        <section className="mt-12">
+        <section className="mt-12 px-5 lg:px-0">
             <div className=" w-full flex justify-between my-6 text-sm ">
                 <p>Created tasks:  <span className="bg-primaryColor px-2 rounded-full text-textColorDark"> {tasksLenght}</span></p>
                 <p>Completed:  <span className=" bg-primaryColor px-2 rounded-full text-textColorDark"> {completedTaskLength} of {tasksLenght}</span></p>
